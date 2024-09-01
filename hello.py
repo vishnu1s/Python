59b8b1bd6e444fd5f5496437f65f8fff4c65afa1
vishnu1s <vishnushinde428@gmail.com>
2024-09-01 10:50:26 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,52 @@
+# # print("hello world")
+
+
+# # for i in range(10):
+# #     print(i)
+
+
+
+# def sum():
+#     c = a + b
+#     return print(c)
+
+# a = int(input("enter first number ->" ))
+# b = int(input("enter second number ->" ))
+
+# sum()
+
+
+# for i in range(5):
+#     for j in range(i,5):
+#         print("*")
+
+# import pandas as pd
+
+# x = int(input("Please enter your number "))
+
+# if x%2 == 0:
+#     print("number is even")
+# else:
+#     print("is odd")    
+
+
+# def is_even(num):
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("odd") 
+
+# is_even(12)           
+
+
+# l = [1,2,3,4,5,6,7]
+
+# for i in l:
+#     if i % 5 != 0 & i % 7 !=0:
+#         print(i)
+
+
+str1 = "Assessment 1 AM 609"
+
+print(str1[-9:-0:-1])
+
